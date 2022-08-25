5bd656b67aec7260bd1c1172b60c2e045f93a831
Moreen <58819299+Weihnachtsfeier@users.noreply.github.com>
2022-08-25 11:27:09 +0200

    set brake lines

--- src/components/movie5/Movie5.js
+++ src/components/movie5/Movie5.js
@@ -19,13 +19,13 @@ const Movie5 = () => {
           In the course of this century the world population will increase to ten billion.
           Where is the food supposed to come from that every individual needs to survive every day, and of which
           every sixth person already has too little. How can we prevent humanity from destroying the basis for its
-          nutrition simply by growing?</p>
-        <p class="card-text">
+          nutrition simply by growing?
+          <br/>
           With his last film "Taste The Waste", director Valentin Thurn showed the immense amounts of food that go
           to waste these days. In doing so, he addressed a broad public and sparked an intensive social debate beyond
           Germany. Now he's going one step further and in his new documentary "10 Billion" focuses on agriculture as
-          the basis of world nutrition.</p>
-        <p class="card-text">
+          the basis of world nutrition.
+          <br/>
           How can enough food be produced for ten billion people in the future? Two camps claim to know the solution:
           On the one hand, industrial agriculture, which is constantly expanding globally and relies on highly efficient
           mass production. On the other hand, there is organic and traditional agriculture, which produce less mass,
@@ -33,8 +33,8 @@ const Movie5 = () => {
           the world in the future. The film shows the global interactions in agriculture using protagonists from the
           central production areas of seeds, fertilization, pest control, animal feed production, animal production and
           trade. The current practice of both sides will be critically questioned, but also their solutions and visions
-          for the future will be presented impartially.</p>
-        <p class="card-text">
+          for the future will be presented impartially.
+          <br/>
           At the end of the film, there are innovative approaches to food security at the local or regional level. They
           all reveal the enormous influence we have with our eating habits. Each of us actively decides which way
           agriculture will dominate in the future. "10 billion" offers a well-founded basis for decision-making.
